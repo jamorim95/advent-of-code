b4041b6e5e4997489af8e0744418443e6ed5fd64
jamorim95 <j.amorim95@hotmail.com>
2022-12-11 00:19:44 +0000

    WIP 2022/8 - part 1

--- 2022/8/script.py
+++ 2022/8/script.py
@@ -7,7 +7,26 @@ def fetch_input(filename: str="input.txt"):
 def main(filename: str="input.txt") -> int:
 	_input = fetch_input(filename=filename)
 
-	return None
+	_input_matrix = build_input_matrix(_input=_input)
+	res = (2*len(_input_matrix)) + (2*len(_input_matrix[0])) - 4
+
+	res += check_inner_forest(_input=_input_matrix)
+
+	return res
+
+def check_inner_forest(_input: list) -> int:
+
+	# TODO: finish
+
+	return 0
+
+def build_input_matrix(_input: list) -> list:
+	res = []
+
+	for r in _input:
+		res.append([int(v) for v in [*r]])
+
+	return res
 
 if __name__ == '__main__':
 	print(main())
